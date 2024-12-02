329d36686d5796e7a608e5f7dfad2a72e3213528
Mostafa Ayed <engmostafaaued31@gmail.com>
2024-12-02 16:41:55 +0200

    edit the login stpes

--- src/test/java/StepDefinitions/loginstpes.java
+++ src/test/java/StepDefinitions/loginstpes.java
@@ -3,16 +3,12 @@ package StepDefinitions;
 
 
 import Pages.LoginPage;
-import io.cucumber.java.en.And;
 
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.concurrent.TimeUnit;
 
 
 public class loginstpes {
@@ -21,27 +17,16 @@ public class loginstpes {
     private WebDriver driver = managers.DriverManager.getDriver();
 
 
-    @And("user is on login page")
-    public void user_is_on_login_page() {
-        driver.get("https://sang.greenboard.online/login");
-System.out.println("mostafa");
-    }
-    @When("user enters valid username and password")
+
+    @When("admin login username and password")
     public void user_enters_valid_username_and_password() {
+
+        driver.get("https://sang.greenboard.online/login");
         LoginPage login=new LoginPage(driver);
         login.performLogin("admin@greenboard.com","string");
 
     }
-    @When("clicks on login button")
-    public void clicks_on_login_button() {
-        // Write code here that turns the phrase above into concrete actions
 
-    }
-    @Then("user is navigated to the home page")
-    public void user_is_navigated_to_the_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-
-    }
 
 
 
--- src/test/resources/Features/login.feature
+++ src/test/resources/Features/login.feature
@@ -2,5 +2,5 @@ Feature: login functionality test
 
   Scenario: Check login with valid credentials
 
-    And user is on login page
-    When user enters valid username and password
+
+    When admin login username and password
